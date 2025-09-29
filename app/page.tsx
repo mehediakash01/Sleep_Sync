193203e9e9abe4bb2852958bea72ea5d4c6bc494
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-09-29 15:10:44 +0600

    added Streaks section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 
 import { Banner } from "@/Components/Banner";
 import Features from "@/Components/Features";
+import Streaks from "@/Components/Streaks";
 import { WhySleepMatters } from "@/Components/WhySleepMatters";
 
 
 export default function Home() {
   return (
-   <div className="bg-gradient-to-tl from-secondary to-primary" >
-<Banner></Banner>
-<WhySleepMatters></WhySleepMatters>
-<Features></Features>
+   <div  >
+<div className="bg-gradient-to-tl from-secondary to-primary">
+  <Banner></Banner>
+  <WhySleepMatters></WhySleepMatters>
+  <Features></Features>
+  <Streaks></Streaks>
+</div>
+
    </div>
   );
 }
