f39ef93bc9027295ec998a8752d60d8bee31b9fc
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-30 23:04:26 +0600

    added animation in banner section

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -1,32 +1,37 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import Container from "./Container";
 import { SleepTrackBtn } from "@/Action/SleepTrackBtn";
-
+import { motion } from "framer-motion";
 export const Banner = () => {
   return (
     <Container className="pt-16">
       <section className="flex flex-col lg:flex-row justify-between items-center  ">
-        <div className="space-y-8">
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="space-y-8"
+        >
           <h1 className=" font-bold text-2xl lg:text-6xl">
-          Build Smarter <br /> Solutions with AI <br /> & Modern Web
+            Build Smarter <br /> Solutions with AI <br /> & Modern Web
           </h1>
-          <p >
-            Monitor your sleep, stay on track, 
-            and unlock <br />AI-powered insights 
-            for deeper, healthier rest.
+          <p>
+            Monitor your sleep, stay on track, and unlock <br />
+            AI-powered insights for deeper, healthier rest.
           </p>
 
-         <SleepTrackBtn></SleepTrackBtn>
-        </div>
-        <div className="lg:mt-6">
+          <SleepTrackBtn></SleepTrackBtn>
+        </motion.div>
+        <motion.div initial={{opacity:0,x:20}} animate={{opacity:1,x:-50}} transition={{duration:0.6}} className="lg:mt-6">
           <Image
             src="/images/heroBg.png"
             alt="girl sleeping in bed"
             width={500}
             height={500}
           ></Image>
-        </div>
+        </motion.div>
       </section>
     </Container>
   );
