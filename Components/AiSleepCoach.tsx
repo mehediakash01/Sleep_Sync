cd09f6f0bda162c9660f7ff086f68e6622f4c80c
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-11-05 00:00:16 +0600

    added animation in ai Sleep coach section

--- Components/AiSleepCoach.tsx
+++ Components/AiSleepCoach.tsx
@@ -1,13 +1,16 @@
-
+"use client"
 import Image from "next/image";
 import Container from "./Container";
 import { AskAiBtn } from "@/Action/AskAiBtn";
-
+import {easeInOut, motion} from "framer-motion"
 const AiSleepCoach = () => {
   return (
     <Container className=" grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
       {/* Left Content */}
-      <div className="text-center lg:text-left">
+      <motion.div  initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: false, amount: 0.3 }}
+        transition={{ duration: 0.6, ease: "easeOut" }} className="text-center lg:text-left">
         <h2 className="text-4xl font-bold mb-4">
           Your Personal <span className="text-blue-500">AI Sleep Coach</span>
         </h2>
@@ -16,7 +19,7 @@ const AiSleepCoach = () => {
           Our AI coach learns from your habits and guides you toward better rest, one night at a time.
         </p>
         <AskAiBtn/>
-      </div>
+      </motion.div>
 
       {/* Right Image */}
       <div className="flex justify-center lg:justify-end">
