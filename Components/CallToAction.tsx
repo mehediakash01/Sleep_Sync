8d2f45303bd01b13db0f130fe16b85b16b2ed053
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-11-11 21:43:52 +0600

    added animation to call to action section

--- Components/CallToAction.tsx
+++ Components/CallToAction.tsx
@@ -1,22 +1,31 @@
+"use client"
 import Image from "next/image";
 import Container from "./Container";
-
+import {motion} from "framer-motion"
 const CallToAction = () => {
   return (
-    <Container className=" grid grid-cols-1 lg:grid-cols-2 gap-12 items-center  text-white ">
+    <Container className=" grid grid-cols-1 lg:grid-cols-2 gap-12 items-center  text-black ">
       {/* Left Side */}
-      <div className="text-center lg:text-left">
+      <motion.div
+      initial={{opacity:0,x:-200}}
+      whileInView={{opacity:1,x:0}}
+      viewport={{once:false,amount:0.3}}
+      transition={{duration:0.6,ease:"easeInOut"}}
+       className="text-center lg:text-left">
         <h2 className="text-4xl font-bold mb-4">Ready to Sleep Smarter?</h2>
         <p className="text-lg mb-6">
           Start tracking your sleep today and build healthier habits for life.
         </p>
-        <button className="px-8 py-4 bg-white text-purple-700 font-semibold rounded-full shadow-lg hover:scale-105 transition">
-          Sign Up Free
-        </button>
-      </div>
+      <button  className="btn   bg-gradient-to-l from-secondary to-primary rounded-full">
+        sign Up free
+          </button>
+      </motion.div>
 
       {/* Right Side Illustration */}
-      <div className="flex justify-center lg:justify-end">
+      <motion.div initial={{opacity:0,x:200}}
+      whileInView={{opacity:1,x:0}}
+      viewport={{once:false,amount:0.3}}
+      transition={{duration:0.6,ease:"easeInOut"}} className="flex justify-center lg:justify-end">
         <Image
           src="/images/bedroom.png"
           alt="Cozy Bedroom"
@@ -24,7 +33,7 @@ const CallToAction = () => {
           height={400}
           
         />
-      </div>
+      </motion.div>
     </Container>
   );
 };
