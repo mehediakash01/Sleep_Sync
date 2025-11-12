c59c577733e8cf71933224805cbb1e221bcd424f
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-11-12 23:32:26 +0600

    fixed dashboard access without login issue

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export  default function Loading () {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-50 via-white to-purple-50">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600 mx-auto mb-4"></div>
+         
+        </div>
+      </div>
+  )
+}
--- app/dashboard/sleepTracking/page.tsx
+++ app/dashboard/sleepTracking/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { SleepTrackForm } from './SleepTrackForm'
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-export default function SleepTrackingPage() {
+export default async function SleepTrackingPage() {
+    const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login?callbackUrl=/dashboard/sleep");
+  }
   return (
     <div >
       <div className='text-center py-6'>
